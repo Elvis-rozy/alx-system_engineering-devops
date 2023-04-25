aa02443f6b8ee85d46118d61fab53f1d783b562d
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-25 14:19:40 +0000

    task 3

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,19 +9,19 @@ package {'nginx':
 
 exec {'install':
 	command	=>'sudo apt-get update ; sudo apt-get -y install nginx',
-	provider => shell,
+	provider	=> shell,
 }
 
 exec {'Hello World':
 	command	=> 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html,
-	provider => shell,
+	provider	=> shell,
 }
 
 exec {'sudo sed -i "s/listen 90 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
-	provider => shell,
+	provider	=> shell,
 }
 
 exec {'run':
 	command	=> 'sudo service nginx restart',
-	provider => shell,
+	provider	=> shell,
 }
