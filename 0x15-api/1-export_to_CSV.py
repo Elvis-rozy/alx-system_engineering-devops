0605165419089b33339b134f83122c40f910fb59
Elvis-rozy <elvisrozy52@gmail.com>
2023-05-22 18:05:00 +0000

    task 3 attempted

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,11 +7,11 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    usr = r.get(url + "users/{}".format(user_id)).json()
-    username = usr.get("username")
+    user = r.get(url + "users/{}".format(user_id)).json()
+    user_name = user.get("username")
     to_do = r.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, elm.get("completed"),
+        [writer.writerow([user_id, user_name, elm.get("completed"),
                           elm.get("title")]) for elm in to_do]
