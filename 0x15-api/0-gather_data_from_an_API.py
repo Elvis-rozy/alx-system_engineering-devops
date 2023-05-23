c277be3225d3754d66b538585f09a5b3cce8263a
Elvis-rozy <elvisrozy52@gmail.com>
2023-05-23 11:04:14 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
-import sys
+'''
+Script to export data in the JSON format.
+'''
+
+import json
+import requests
+from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    complete = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(complete)
-    print("Employee {} is done with tasks({}/{}):".format(user_id.get("name"),
-                                                          len(complete),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in complete]
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
+    todo = requests.get(url, verify=False).json()
+    name = user.get('username')
+    t = [{"task": t.get("title"),
+          "username": name,
+          "completed": t.get("completed")} for t in todo]
+    bj = {}
+    bj[uid] = t
+    with open("{}.json".format(uid), 'w') as filejs:
+        json.dump(bj, filejs)
