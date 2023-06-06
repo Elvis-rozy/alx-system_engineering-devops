39ad910583875814a79fceea9895fb101adf71d1
Elvis-rozy <elvisrozy52@gmail.com>
2023-06-06 13:26:18 +0000

    task 3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,13 +9,13 @@ def top_ten(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
+    parameters = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get(url, headers=headers, parameters=parameters,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    result = response.json().get("data")
+    [print(c.get("data").get("title")) for c in result.get("children")]
